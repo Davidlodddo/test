6ff2b6e8c1609e6803866b0ea42edbb68f3b1623
David <davidoloddo@gmail.com>
2025-09-22 16:47:13 +0100

    all

--- script.js
+++ script.js
@@ -1,5 +1,49 @@
+    const modal = document.getElementById('otpModal');
 
+    const otpInput = document.getElementById('otpInput');
+
+  otpInput.addEventListener('input', function () {
+    this.value = this.value.replace(/\D/g, ''); // Remove non-digit characters
+  });
+
+window.onclick = function(event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+      document.getElementById('otpInput').value = ""
+    }
+
+  };
+
+document.getElementById("loginForm").addEventListener("submit", (event) => {
+  event.preventDefault();
+  modal.style.display = 'block'; // Show OTP modal
+  // alert("working")
+  //  let username = document.getElementById('username').value;
+  // if (username === "abcny1") {
+  //   let test = document.getElementById("logInPage");
+  // test.style.display = "none";
+  // document.getElementById("loaderContainer").style.display = "flex";
+  // // document.getElementById("accountPage").style.display = "block";
+  // setTimeout(() => {
+  //   document.querySelector(".loaderContainer").style.display = "none";
+  //   document.getElementById("accountPage").style.display = "block";
+  //   document.getElementById("accountPage").style.opacity = "1";
+  // }, 2000);
+  // } else{
+  //    let test = document.getElementById("logInPage");
+  // test.style.display = "none";
+  // document.getElementById("loaderContainer").style.display = "flex";
+  // // document.getElementById("accountPage").style.display = "block";
+  // setTimeout(() => {
+  //   document.querySelector(".loaderContainer").style.display = "none";
+  //   document.getElementById("accountPage2").style.display = "block";
+  //   document.getElementById("accountPage2").style.opacity = "1";
+  // }, 2000);
+  // }
+  // return
+})
 const handleLogin = () => {
+  modal.style.display = "none";
   let username = document.getElementById('username').value;
   if (username === "abcny1") {
     let test = document.getElementById("logInPage");
